1b8b158b7298ffb086f9e2dadc82655695338a1f
Harry Hopkinson <harryjhopkinson@gmail.com>
2025-04-07 11:49:22 +0100

    Merge build scripts

--- scripts/config-release.sh
+++
@@ -1,2 +0,0 @@
-git switch save-config
-g++ -static -O2 -s -o config.exe src/main.cpp -lgdi32 -luser32 -lkernel32 -mwindows
--- scripts/minimal-release.sh
+++
@@ -1,2 +0,0 @@
-git switch main
-g++ -static -O2 -s -o minimal.exe src/main.cpp -lgdi32 -luser32 -lkernel32 -mwindows

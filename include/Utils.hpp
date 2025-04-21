c08ef5fafb9c20b427b8da1527722343d9c6cca2
Harry Hopkinson <harryjhopkinson@gmail.com>
2025-04-21 18:46:59 +0100

    Format code to consistent style

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 template <typename T>
-constexpr const T& clamp(const T& value, const T& low, const T& high) {
+constexpr const T& clamp(const T& value, const T& low, const T& high)
+{
     if (value < low) return low;
     if (value > high) return high;
     return value;

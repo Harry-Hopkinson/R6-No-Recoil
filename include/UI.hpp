0436d3351f236aa967244ce51e7c84ea59ec2390
Harry Hopkinson <harryjhopkinson@gmail.com>
2025-04-23 16:23:01 +0100

    Move Buttons class to Ui.hpp

--- include/UI.hpp
+++ include/UI.hpp
@@ -1,7 +1,28 @@
 #pragma once
 
-#include "../include/Button.hpp"
+#include <string>
 #include <vector>
+
+const int WINDOW_WIDTH = 650;
+const int WINDOW_HEIGHT = 400;
+
+class Button
+{
+    public:
+        Button(HWND parent, int x, int y, int width, int height, const std::string& text, int id)
+            : hwndButton(nullptr), id(id)
+        {
+            hwndButton = CreateWindow(
+                "BUTTON", text.c_str(),
+                WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+                x, y, width, height,
+                parent, (HMENU)(uintptr_t)id, GetModuleHandle(NULL), NULL);
+        }
+    private:
+        HWND hwndButton;
+        int id;
+};
+std::vector<Button> Buttons;
 
 // Function to center text
 void DrawCenteredText(HDC hdc, LPCSTR text, int yOffset, int windowWidth)
@@ -12,12 +33,7 @@ void DrawCenteredText(HDC hdc, LPCSTR text, int yOffset, int windowWidth)
     TextOut(hdc, textX, yOffset, text, strlen(text));
 }
 
-std::vector<Button> Buttons;
-const int WINDOW_WIDTH = 650;
-const int WINDOW_HEIGHT = 400;
-
 bool DarkTheme = true;
-
 void ToggleTheme()
 {
     DarkTheme = !DarkTheme;

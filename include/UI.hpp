d97d594ddc89909d8920ba8645780853aa3c7b4a
Harry Hopkinson <harryjhopkinson@gmail.com>
2025-04-23 17:59:58 +0100

    Turn text Button parameter to const char

--- include/UI.hpp
+++ include/UI.hpp
@@ -9,11 +9,11 @@ const int WINDOW_HEIGHT = 400;
 class Button
 {
     public:
-        Button(HWND parent, int x, int y, int width, int height, const std::string& text, int id)
+        Button(HWND parent, int x, int y, int width, int height, const char* text, int id)
             : hwndButton(nullptr), id(id)
         {
             hwndButton = CreateWindow(
-                "BUTTON", text.c_str(),
+                "BUTTON", text,
                 WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
                 x, y, width, height,
                 parent, (HMENU)(uintptr_t)id, GetModuleHandle(NULL), NULL);

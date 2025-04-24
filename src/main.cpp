3c3b39ba6f3eff5db9a16c046db0d9cf45d45266
Harry Hopkinson <harryjhopkinson@gmail.com>
2025-04-24 09:45:17 +0100

    Return back to original formatting

--- src/main.cpp
+++ src/main.cpp
@@ -136,7 +136,6 @@ void ApplyRecoil()
                 std::this_thread::sleep_for(std::chrono::milliseconds(5));
             }
         }
-
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 }
